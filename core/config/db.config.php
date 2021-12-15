473b935ca0821d7a23b209612bf7b6a30c0ed325
Mandeep <1mandeep2021@gmail.com>
2021-12-15 09:49:01 +0530

    code push

---
+++ core/config/db.config.php
@@ -0,0 +1,33 @@
+<?php
+defined('ROOT_DIR') or die(header('HTTP/1.1 403 Forbidden'));
+
+/*
+ * @author Balaji
+ * @name: Rainbow PHP Framework
+ * @copyright 2020 ProThemes.Biz
+ *
+ */
+
+// --- Database Settings ---
+
+// Database Hostname
+$dbHost = 'localhost';
+
+// Database Username
+$dbUser = 'mandeep';
+
+// Database Password
+$dbPass = 'admin0066';
+
+// Database Name
+$dbName = 'seotool';
+
+//Base URL (Without http:// & https://)
+$baseURL = 'localhost/stool/';
+
+//Item Purchase Code
+$item_purchase_code = 'New-SDFE-LERJJDF-KSDRser';
+
+//Domain Security Code
+$authCode = '2bcf8aa4097bf5cc28fe19596c8cf812';
+
