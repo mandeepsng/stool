26dd7e07ddf599d55762e793e169fba1303bb703
Mandeep <1mandeep2021@gmail.com>
2021-12-15 09:57:26 +0530

    upload db

---
+++ database/seotool.sql
@@ -0,0 +1,211 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 15, 2021 at 05:25 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `seotool`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+-- Error reading structure for table seotool.admin: #1932 - Table 'seotool.admin' doesn't exist in engine
+-- Error reading data for table seotool.admin: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`admin`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_history`
+--
+-- Error reading structure for table seotool.admin_history: #1932 - Table 'seotool.admin_history' doesn't exist in engine
+-- Error reading data for table seotool.admin_history: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`admin_history`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ads`
+--
+-- Error reading structure for table seotool.ads: #1932 - Table 'seotool.ads' doesn't exist in engine
+-- Error reading data for table seotool.ads: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`ads`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banned_ip`
+--
+-- Error reading structure for table seotool.banned_ip: #1932 - Table 'seotool.banned_ip' doesn't exist in engine
+-- Error reading data for table seotool.banned_ip: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`banned_ip`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `capthca`
+--
+-- Error reading structure for table seotool.capthca: #1932 - Table 'seotool.capthca' doesn't exist in engine
+-- Error reading data for table seotool.capthca: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`capthca`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ddos`
+--
+-- Error reading structure for table seotool.ddos: #1932 - Table 'seotool.ddos' doesn't exist in engine
+-- Error reading data for table seotool.ddos: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`ddos`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `interface`
+--
+-- Error reading structure for table seotool.interface: #1932 - Table 'seotool.interface' doesn't exist in engine
+-- Error reading data for table seotool.interface: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`interface`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lang`
+--
+-- Error reading structure for table seotool.lang: #1932 - Table 'seotool.lang' doesn't exist in engine
+-- Error reading data for table seotool.lang: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`lang`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mail`
+--
+-- Error reading structure for table seotool.mail: #1932 - Table 'seotool.mail' doesn't exist in engine
+-- Error reading data for table seotool.mail: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`mail`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mail_templates`
+--
+-- Error reading structure for table seotool.mail_templates: #1932 - Table 'seotool.mail_templates' doesn't exist in engine
+-- Error reading data for table seotool.mail_templates: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`mail_templates`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pages`
+--
+-- Error reading structure for table seotool.pages: #1932 - Table 'seotool.pages' doesn't exist in engine
+-- Error reading data for table seotool.pages: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`pages`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `plugins`
+--
+-- Error reading structure for table seotool.plugins: #1932 - Table 'seotool.plugins' doesn't exist in engine
+-- Error reading data for table seotool.plugins: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`plugins`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pr02`
+--
+-- Error reading structure for table seotool.pr02: #1932 - Table 'seotool.pr02' doesn't exist in engine
+-- Error reading data for table seotool.pr02: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`pr02`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pr24`
+--
+-- Error reading structure for table seotool.pr24: #1932 - Table 'seotool.pr24' doesn't exist in engine
+-- Error reading data for table seotool.pr24: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`pr24`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rainbowphp_temp`
+--
+-- Error reading structure for table seotool.rainbowphp_temp: #1932 - Table 'seotool.rainbowphp_temp' doesn't exist in engine
+-- Error reading data for table seotool.rainbowphp_temp: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`rainbowphp_temp`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rainbow_track`
+--
+-- Error reading structure for table seotool.rainbow_track: #1932 - Table 'seotool.rainbow_track' doesn't exist in engine
+-- Error reading data for table seotool.rainbow_track: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`rainbow_track`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recent_history`
+--
+-- Error reading structure for table seotool.recent_history: #1932 - Table 'seotool.recent_history' doesn't exist in engine
+-- Error reading data for table seotool.recent_history: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`recent_history`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seo_tools`
+--
+-- Error reading structure for table seotool.seo_tools: #1932 - Table 'seotool.seo_tools' doesn't exist in engine
+-- Error reading data for table seotool.seo_tools: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`seo_tools`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `site_info`
+--
+-- Error reading structure for table seotool.site_info: #1932 - Table 'seotool.site_info' doesn't exist in engine
+-- Error reading data for table seotool.site_info: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`site_info`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `themes_data`
+--
+-- Error reading structure for table seotool.themes_data: #1932 - Table 'seotool.themes_data' doesn't exist in engine
+-- Error reading data for table seotool.themes_data: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`themes_data`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table seotool.users: #1932 - Table 'seotool.users' doesn't exist in engine
+-- Error reading data for table seotool.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`users`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_input_history`
+--
+-- Error reading structure for table seotool.user_input_history: #1932 - Table 'seotool.user_input_history' doesn't exist in engine
+-- Error reading data for table seotool.user_input_history: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`user_input_history`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_settings`
+--
+-- Error reading structure for table seotool.user_settings: #1932 - Table 'seotool.user_settings' doesn't exist in engine
+-- Error reading data for table seotool.user_settings: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `seotool`.`user_settings`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
